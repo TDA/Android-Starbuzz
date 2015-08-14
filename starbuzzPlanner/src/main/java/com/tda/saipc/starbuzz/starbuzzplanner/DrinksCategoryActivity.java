1d239d064adc3a1f0fae7d1181686cc8c8cc5097
Sai Pc <schand31@asu.edu>
2015-08-14 14:55:48 -0700

    added list adapter to programmatically add an array of drinks

--- starbuzzPlanner/src/main/java/com/tda/saipc/starbuzz/starbuzzplanner/DrinksCategoryActivity.java
+++ starbuzzPlanner/src/main/java/com/tda/saipc/starbuzz/starbuzzplanner/DrinksCategoryActivity.java
@@ -5,6 +5,7 @@ import android.app.ListActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
 
@@ -13,7 +14,10 @@ public class DrinksCategoryActivity extends ListActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        // takes context: this, layout_style, array: drinks
+        ArrayAdapter<Drink> listAdapter = new ArrayAdapter<Drink>(this, android.R.layout.simple_list_item_1, Drink.drinks);
         ListView listView = this.getListView();
-        listView.
+        listView.setAdapter(listAdapter);
+        listView
     }
 }
