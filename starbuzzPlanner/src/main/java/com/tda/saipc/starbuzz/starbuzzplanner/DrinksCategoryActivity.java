5ec627a859990c3141e9382198a9ca82f247e858
Sai Pc <schand31@asu.edu>
2015-08-14 14:48:06 -0700

    Added drinks category activity
    Added an intent from top -> drinks

---
+++ starbuzzPlanner/src/main/java/com/tda/saipc/starbuzz/starbuzzplanner/DrinksCategoryActivity.java
@@ -0,0 +1,19 @@
+package com.tda.saipc.starbuzz.starbuzzplanner;
+
+import android.app.Activity;
+import android.app.ListActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.ListView;
+
+
+public class DrinksCategoryActivity extends ListActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        ListView listView = this.getListView();
+        listView.
+    }
+}
--- starbuzzPlanner/src/main/java/com/tda/saipc/starbuzz/starbuzzplanner/TopLevelActivity.java
+++ starbuzzPlanner/src/main/java/com/tda/saipc/starbuzz/starbuzzplanner/TopLevelActivity.java
@@ -1,9 +1,13 @@
 package com.tda.saipc.starbuzz.starbuzzplanner;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
 
 
 public class TopLevelActivity extends Activity {
@@ -12,28 +16,17 @@ public class TopLevelActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_top_level);
-    }
-
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_top_level, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
+        AdapterView.OnItemClickListener itemClickListener = new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                if(position == 0) {
+                    // the drinks were clicked, pull up DrinksCategoryActivity with an intent
+                    Intent intent = new Intent(TopLevelActivity.this, DrinksCategoryActivity.class);
+                    startActivity(intent);
+                }
+            }
+        };
+        ListView listView = (ListView) findViewById(R.id.listOfDrinks);
+        listView.setOnItemClickListener(itemClickListener);
     }
 }
